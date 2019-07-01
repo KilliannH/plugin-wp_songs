033b6b1b463ba11f2740ee7d777fb2f6ec5ddf88
KilliannH <killiannhervagault@gmail.com>
2019-07-01 23:05:21 +0200

    register / unregister post type

--- plugin-wp_songs.php
+++ plugin-wp_songs.php
@@ -6,8 +6,16 @@
 
 // Activation hook
 function wp_songs_setup_post_type() {
-	// register the "book" custom post type
-	register_post_type( 'book', ['public' => 'true'] );
+	register_post_type('wporg_product',
+		array(
+			'labels'      => array(
+				'name'          => __('Products'),
+				'singular_name' => __('Product'),
+			),
+			'public'      => true,
+			'has_archive' => true,
+		)
+	);
 }
 add_action( 'init', 'wp_songs_setup_post_type' );
 
@@ -24,7 +32,7 @@ register_activation_hook( __FILE__, 'wp_songs_install' );
 // Deactivation hook
 function wp_songs_deactivation() {
 	// unregister the post type, so the rules are no longer in memory
-	unregister_post_type( 'book' );
+	unregister_post_type( 'wporg_product' );
 	// clear the permalinks to remove our post type's rules from the database
 	flush_rewrite_rules();
 }
